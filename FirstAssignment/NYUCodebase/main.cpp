d4dc8e719ce13245145e47eafb1a387286b12241
richgoldfeld <richgoldfeld@gmail.com>
2015-09-22 16:51:24 -0400

    Added setup files for first assignment

---
+++ FirstAssignment/NYUCodebase/main.cpp
@@ -0,0 +1,74 @@
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include <SDL_image.h>
+#include "ShaderProgram.h"
+#include "Matrix.h"
+
+#ifdef _WINDOWS
+#define RESOURCE_FOLDER ""
+#else
+#define RESOURCE_FOLDER "NYUCodebase.app/Contents/Resources/"
+#endif
+
+SDL_Window* displayWindow;
+
+GLuint LoadTexture(const char *image_path) {
+	SDL_Surface *surface = IMG_Load(image_path);
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface->w, surface->h, 0, GL_RGBA,
+		GL_UNSIGNED_BYTE, surface->pixels);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	SDL_FreeSurface(surface);
+	return textureID;
+}
+int main(int argc, char *argv[])
+{
+	SDL_Init(SDL_INIT_VIDEO);
+	displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 360, SDL_WINDOW_OPENGL);
+	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
+	SDL_GL_MakeCurrent(displayWindow, context);
+#ifdef _WINDOWS
+	glewInit();
+#endif
+	glViewport(0, 0, 640, 360);
+	ShaderProgram program(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
+	Matrix projectionMatrix;
+	Matrix modelMatrix;
+	Matrix viewMatrix;
+	projectionMatrix.setOrthoProjection(-3.55, 3.55, -2.0f, 2.0f, -1.0f, 1.0f);
+	SDL_Event event;
+	bool done = false;
+	glClearColor(0.4f, 0.2f, 0.8f, 1.0f);
+	LoadTexture("smiley1.png");
+	while (!done) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+				done = true;
+			}
+		}
+
+		program.setModelMatrix(modelMatrix);
+		program.setProjectionMatrix(projectionMatrix);
+		program.setViewMatrix(viewMatrix);
+		glUseProgram(program.programID);
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		float vertices[] = { 0.5f, -0.5f, 0.0f, 0.5f, -0.5f, -0.5f };
+		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+		glEnableVertexAttribArray(program.positionAttribute);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDisableVertexAttribArray(program.positionAttribute);
+
+		SDL_GL_SwapWindow(displayWindow);
+	}
+
+	SDL_Quit();
+	return 0;
+}
