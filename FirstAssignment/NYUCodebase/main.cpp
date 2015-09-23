58ec360d3e7fc00e4ac2832feb51a9446c262dab
richgoldfeld <richgoldfeld@gmail.com>
2015-09-22 22:11:30 -0400

    Updated code to working version with 3 sprites that all rotate

--- FirstAssignment/NYUCodebase/main.cpp
+++ FirstAssignment/NYUCodebase/main.cpp
@@ -6,6 +6,7 @@
 #include <SDL_image.h>
 #include "ShaderProgram.h"
 #include "Matrix.h"
+#include <iostream>
 
 #ifdef _WINDOWS
 #define RESOURCE_FOLDER ""
@@ -20,8 +21,7 @@ GLuint LoadTexture(const char *image_path) {
 	GLuint textureID;
 	glGenTextures(1, &textureID);
 	glBindTexture(GL_TEXTURE_2D, textureID);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface->w, surface->h, 0, GL_RGBA,
-		GL_UNSIGNED_BYTE, surface->pixels);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface->w, surface->h, 0, GL_RGBA,	GL_UNSIGNED_BYTE, surface->pixels);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	SDL_FreeSurface(surface);
@@ -30,42 +30,74 @@ GLuint LoadTexture(const char *image_path) {
 int main(int argc, char *argv[])
 {
 	SDL_Init(SDL_INIT_VIDEO);
-	displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 360, SDL_WINDOW_OPENGL);
+	displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL);
 	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
 	SDL_GL_MakeCurrent(displayWindow, context);
 #ifdef _WINDOWS
 	glewInit();
 #endif
+	float lastFrameTicks = 0.0f;
 	glViewport(0, 0, 640, 360);
 	ShaderProgram program(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
 	Matrix projectionMatrix;
 	Matrix modelMatrix;
+	modelMatrix.Rotate(180 * (3.1415926 / 180.0));
+	size_t angle = 0;
 	Matrix viewMatrix;
 	projectionMatrix.setOrthoProjection(-3.55, 3.55, -2.0f, 2.0f, -1.0f, 1.0f);
 	SDL_Event event;
 	bool done = false;
 	glClearColor(0.4f, 0.2f, 0.8f, 1.0f);
-	LoadTexture("smiley1.png");
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	while (!done) {
 		while (SDL_PollEvent(&event)) {
 			if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
 				done = true;
 			}
 		}
-
 		program.setModelMatrix(modelMatrix);
 		program.setProjectionMatrix(projectionMatrix);
 		program.setViewMatrix(viewMatrix);
 		glUseProgram(program.programID);
 
 		glClear(GL_COLOR_BUFFER_BIT);
-
-		float vertices[] = { 0.5f, -0.5f, 0.0f, 0.5f, -0.5f, -0.5f };
-		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+		
 		glEnableVertexAttribArray(program.positionAttribute);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glEnableVertexAttribArray(program.texCoordAttribute);
+		float vertices[] = { 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f};
+		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+		glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, vertices);
+		LoadTexture("smiley1.png");
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDisableVertexAttribArray(program.texCoordAttribute);
+		glDisableVertexAttribArray(program.positionAttribute);
+		
+		glEnableVertexAttribArray(program.positionAttribute);
+		glEnableVertexAttribArray(program.texCoordAttribute);
+		float vertices2[] = { 2.0f, 2.0f, 1.0f, 1.0f, 2.0f, 1.0f, 2.0f, 2.0f, 1.0f, 2.0f, 1.0f, 1.0f };
+		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices2);
+		glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, vertices2);
+		LoadTexture("smiley2.png");
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDisableVertexAttribArray(program.texCoordAttribute);
+		glDisableVertexAttribArray(program.positionAttribute);
+		
+		glEnableVertexAttribArray(program.positionAttribute);
+		glEnableVertexAttribArray(program.texCoordAttribute);
+		float vertices3[] = { 3.0f, 1.0f, 2.0f, 0.0f, 3.0f, 0.0f, 3.0f, 1.0f, 2.0f, 1.0f, 2.0f, 0.0f };
+		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices3);
+		glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, vertices3);
+		LoadTexture("smiley3.png");
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDisableVertexAttribArray(program.texCoordAttribute);
 		glDisableVertexAttribArray(program.positionAttribute);
 
+		float ticks = (float)SDL_GetTicks() / 100.0f;
+		float elapsed = ticks - lastFrameTicks;
+		lastFrameTicks = ticks;
+		angle += elapsed;
+		modelMatrix.Rotate(angle * .1);
 		SDL_GL_SwapWindow(displayWindow);
 	}
 
